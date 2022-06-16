b3577468a12a9eee68a0019a0deade1aac242194
anggi.hasian.ft20@mail.umy.ac.id <KAMPUNGbilah1112>
2022-06-16 17:41:05 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection koneksi = new SqlConnection(@"Data Source=ANGGIMUNTHE;Initial Catalog=Siswa;Integrated Security=True");
+       
+       
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Jenis_kelamin;
+        string imglocation = "";
+        SqlCommand cmd;
+             
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] images = null;
+            FileStream stream = new FileStream(imglocation, FileMode.Open, FileAccess.Read);
+            BinaryReader brs = new BinaryReader(stream);
+            images = brs.ReadBytes((int)stream.Length);
+            koneksi.Open();
+            SqlCommand cmd = koneksi.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into [Siswa] (NIS,Nama,Kelas,Jurusan,Tempat_lahir,Jenis_kelamin,Tanggal_lahir,Alamat,Foto )values ('"+textBox1.Text+ "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox5.Text + "','" + textBox4.Text + "','"+dateTimePicker1.Text+"','"+Jenis_kelamin+"','"+textBox6.Text+"',@images)";
+            cmd.Parameters.Add(new SqlParameter("@images", images));
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            dateTimePicker1.Value = DateTime.Now;
+            pictureBox1.ImageLocation = null;
+             MessageBox.Show("Data Insert Successfully");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'siswaDataSet.Siswa' table. You can move, or remove it, as needed.
+            this.siswaTableAdapter.Fill(this.siswaDataSet.Siswa);
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Jenis_kelamin = "Pria";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Jenis_kelamin = "Wanita";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "png files (* .png)|* .png|jpg files(* .jpg)|* .jpg|All files(*.*)|*.*";
+            if (dialog.ShowDialog()==DialogResult.OK)
+            {
+                imglocation = dialog.FileName.ToString();
+                pictureBox1.ImageLocation = imglocation;
+            }
+
+        }
+        public void displaydata()
+        {
+            koneksi.Open();
+            SqlCommand cmd = koneksi.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from [Siswa]";
+            cmd.ExecuteNonQuery();
+            DataTable dta = new DataTable();
+            SqlDataAdapter dataadp = new SqlDataAdapter(cmd);
+            dataadp.Fill(dta);
+            dataGridView1.DataSource = dta;
+            koneksi.Close();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            displaydata();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            SqlCommand cmd = koneksi.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from [siswa] where NIS ='" + textBox1.Text + "'";
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+            textBox1.Text = "'";
+            displaydata();
+            MessageBox.Show("Data delete successfully");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            byte[] images = null;
+            FileStream stream = new FileStream(imglocation, FileMode.Open, FileAccess.Read);
+            BinaryReader brs = new BinaryReader(stream);
+            images = brs.ReadBytes((int)stream.Length);
+            koneksi.Open();
+            SqlCommand cmd = koneksi.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into [Siswa] (NIS,Nama,Kelas,Jurusan,Tempat_lahir,Jenis_kelamin,Tanggal_lahir,Alamat,Foto )values ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox5.Text + "','" + textBox4.Text + "','" + dateTimePicker1.Text + "','" + Jenis_kelamin + "','" + textBox6.Text + "',@images)";
+            cmd.Parameters.Add(new SqlParameter("@images", images));
+            cmd.ExecuteNonQuery();
+            koneksi.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            dateTimePicker1.Value = DateTime.Now;
+            pictureBox1.ImageLocation = null;
+            MessageBox.Show("Data update Successfully");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            koneksi.Open();
+            SqlCommand cmd = koneksi.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText="selec*from[Siswa] where Nama= '"+textBox7+"'";
+            DataTable dta = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dta);
+            dataGridView1.DataSource = dta;
+            koneksi.Close();
+            textBox7.Text = "";
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            dateTimePicker1.Value = DateTime.Now;
+            pictureBox1.ImageLocation = null;
+        }
+    }
+}
